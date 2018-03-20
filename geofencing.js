5494559c2ad4a94b85efd31e3d8849a2adb79170
tanbur <richard.tanburn@gmail.com>
2018-03-20 17:21:01 +0000

    Final commit of first example

--- geofencing.js
+++ geofencing.js
@@ -1,7 +1,6 @@
 function inside(point, vs) {
   // ray-casting algorithm based on
   // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
-
   var x = point[0], y = point[1];
 
   var inside = false;
